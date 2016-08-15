60e9183cd9bc773296d5a3769fa5b79aeb94904d
Arun Sirrpi <aru.arun.dev@gmail.com>
2016-08-15 11:35:31 +1000

    Initial Commit

---
+++ WebtrendsInsideFramework/WebtrendsInsideFramework.h
@@ -0,0 +1,19 @@
+//
+//  WebtrendsInsideFramework.h
+//  WebtrendsInsideFramework
+//
+//  Created by Arun Sirrpi on 15/08/2016.
+//  Copyright © 2016 Australian Broadcasting Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebtrendsInsideFramework.
+FOUNDATION_EXPORT double WebtrendsInsideFrameworkVersionNumber;
+
+//! Project version string for WebtrendsInsideFramework.
+FOUNDATION_EXPORT const unsigned char WebtrendsInsideFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebtrendsInsideFramework/PublicHeader.h>
+
+
